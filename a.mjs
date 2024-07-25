a4dc114f2a73fdce51d09c76e2d31d73e99e9989
Shadow <pieroramirez810@gmail.com>
2024-07-24 21:44:33 -0500

    w

--- a.mjs
+++ a.mjs
@@ -2,6 +2,8 @@ import { a } from './b.mjs'
 import { readdirSync } from 'node:fs'
 console.log(a())
 
+console.log({args: process.argv})
+
 console.log('redingg', readdirSync('.', {
     encoding: 'utf8'
 }))
@@ -10,5 +12,4 @@ console.log(readdirSync(process.argv[1], {
     encoding: 'utf8'
 }))
 
-console.log({args: process.argv})
 
